2c176b2cc970111bf92db1bfd698ecebc61f48b9
Luke <apple894894@gmail.com>
2021-01-30 02:46:53 +0800

    update order modal options

--- src/components/OrderModal.js
+++ src/components/OrderModal.js
@@ -7,15 +7,24 @@ export default function OrderModal(props) {
     const[size, setSize] = useState("");
     const[sugar, setSugar] = useState("");
     const[ice, setIce] = useState("");
+    const[flavor, setFlavor] = useState("");
 
     const appContext = useContext(AppContext);
     const sugarLevels = appContext.drinkData.sugar;
-    const iceLevels = appContext.drinkData.ice;
+    let iceLevels = appContext.drinkData.ice;
+
+    if(!props.data.cold){
+        iceLevels = iceLevels.filter((e) => {return e["id"] !== "2" && e["id"] !== "3"});
+    }
+
+    if(!props.data.hot){
+        iceLevels = iceLevels.filter((e) => {return e["id"] !== "1"});
+    }
 
     return(
         <div className="full-window popup-background" style={{position: "fixed", display: "block", backgroundColor: "rgba(0,0,0,0.25)"}}>
             <div className="modal" style={{position: "fixed", display: "block"}}>
-                <div className="modal-dialog">
+                <div className="modal-dialog modal-dialog-scrollable">
                     <div className="modal-content">
                     <div className="modal-header">
                         <h5 className="modal-title">{"訂購一杯"+props.data.item}</h5>
@@ -23,26 +32,45 @@ export default function OrderModal(props) {
                     </div>
                     <div className="modal-body">
                         <div className="my-2">
-                            <p>大小</p>
+                            <span className="mb-2">大小</span>
                             <div className="select-size">
                                 <SingleOptionButtonGroup title="size" options={Object.keys(props.data.prices)} onChange={setSize} />
                             </div>
                         </div>
                         <div className="my-2">
-                            <p>溫度</p>
+                            <span>溫度</span>
+                            {(!props.data.cold || !props.data.hot) &&
+                                <span style={{color: "gray"}}><small>
+                                    *本飲料不提供
+                                    {!props.data.cold && "冷飲"}
+                                    {!props.data.hot && "熱飲"}
+                                </small></span>
+                            }
                             <div>
                                 <SingleOptionButtonGroup title="ice" options={iceLevels.map(iceLevel => iceLevel["tag"])} onChange={setIce} />
                             </div>
                         </div>
                         <div className="my-2">
-                        <p>甜度</p>
+                            <span>甜度</span>
                             <div className="select-ice">
                                 <SingleOptionButtonGroup title="sugar" options={sugarLevels.map(sugarLevel => sugarLevel["tag"])} onChange={setSugar} />
                             </div>
                         </div>
+                        {props.data.flavor &&
+                            <div className="my-2">
+                                <span>口味</span>
+                                <div className="select-flavor">
+                                    <SingleOptionButtonGroup title="flavor" options={props.data.flavor} onChange={setFlavor} />
+                                </div>
+                            </div>
+                        }
+                        <div className="my-2">
+                            <span>訂購人</span>
+                            <input type="text" className="mt-2 form-control" placeholder="你的名字" aria-label="Username" aria-describedby="basic-addon1" />
+                        </div>
                     </div>
                     <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" onClick={()=>{props.setShowModal(false);}}>取消</button>
+                        {/* <button type="button" className="btn btn-secondary" onClick={()=>{props.setShowModal(false);}}>取消</button> */}
                         <button type="button" className="btn btn-primary">送出訂單</button>
                     </div>
                     </div>
--- src/components/SingleOptionButtonGroup.js
+++ src/components/SingleOptionButtonGroup.js
@@ -2,18 +2,15 @@ import React, { useState } from "react";
 
 export default function SingleOptionButtonGroup(props) {
     return(
-        <div className="btn-group w-100" role="group" aria-label="Basic radio toggle button group">
+        <div className="btn-group w-100 mt-2" role="group" aria-label="Basic radio toggle button group">
             {props.options.map((option, key)=>{
                 return(
-                    <>
-                        <input type="radio" className="btn-check" name={`btn-radio-${props.title}`} id={`btn-radio-${props.title}-${key}`} autocomplete="off" />
-                        <label className="btn btn-outline-primary" for={`btn-radio-${props.title}-${key}`} >
+                    <React.Fragment key={key}>
+                        <input type="radio" className="btn-check" name={`btn-radio-${props.title}`} id={`btn-radio-${props.title}-${key}`} autoComplete="off" />
+                        <label className="btn btn-outline-primary" htmlFor={`btn-radio-${props.title}-${key}`} >
                             {option}
                         </label>
-                        {/* <button key={key} type="button" class={"btn btn-outline-primary"} onClick={()=>{props.onChange(option)}}>
-                            {option}
-                        </button> */}
-                    </>
+                    </React.Fragment>
                 )
             })}
         </div>
