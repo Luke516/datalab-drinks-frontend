ac07b998319dbf22df8efdc8fce27e68fe09400b
Luke <apple894894@gmail.com>
2021-02-19 15:20:26 +0800

    add footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./main.scss";
+import "./App.css";
 import React, {Suspense, useEffect, useState} from 'react';
 import {
   BrowserRouter as Router,
@@ -59,6 +60,14 @@ function App() {
         </Suspense>
       </Router>
     </div>
+    <footer className="bd-footer p-3 mt-5 bg-light text-center text-sm-start">
+      <div className="container">
+        <ul className="bd-footer-links ps-0 mb-3">
+          <li className="d-inline-block"><a href="https://github.com/jackraken/datalab-drinks-frontend">GitHub</a></li>
+        </ul>
+        <p className="mb-0">Designed and built with all the love in the world by the <a href="http://www.cs.nthu.edu.tw/~shwu/">Datalab</a> with the help of <a href="https://www.camacafe.com/">cama coffee</a>(03)571-1500.</p>
+      </div>
+    </footer>
     </AppContext.Provider>
   );
 }
