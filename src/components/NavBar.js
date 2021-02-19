90793a4ad6f3e835acf9d11a5f5913faa702caac
Luke <apple894894@gmail.com>
2021-02-19 14:25:00 +0800

    responsive nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,33 +9,46 @@ export default function NavBar(props) {
     let path = location.pathname.split("/");
 
     return(
-        <div className="nav nav-pills p-2" id="nav-bar" role="tablist">
-            <div id="datalab-drink-logo-background" style={{backgroundImage: `url(${logoBgImg})`}}>
+        <nav className="navbar navbar-light navbar-expand-lg p-1" id="nav-bar" role="tablist">
+            <div id="datalab-drink-logo-background" className="navbar-brand py-2" style={{backgroundImage: `url(${logoBgImg})`}}>
                 <div id="datalab-drink-logo">
                     <img src={dataLabLogo} style={{height: "2.5rem"}}></img>
                 </div>
             </div>
-            <div className={"nav-link-background " + (path[2] == "all"? " selected" : "")}>
-                <Link className="nav-link" to={`/drinks/all`}>
-                    全部飲料
-                </Link>
-            </div>
-            {props.drinkData && props.drinkData.menu &&
-                props.drinkData.menu.map((series, key) => {
-                    return (
-                        <div key={key} className={"nav-link-background" + (path[2] == key? " selected" : "")}>
-                            <Link className={"nav-link"} to={`/drinks/${key}`}>
-                                {series.series}
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <div className={"nav-link-background " + (path[2] == "all"? " selected" : "")}>
+                            <Link className="nav-link" to={`/drinks/all`}>
+                                全部飲料
                             </Link>
                         </div>
-                    )
-                })
-            }
-            <div className={"nav-link-background " + (path[1] == "orders"? " selected" : "")}>
-                <Link className="nav-link" to={`/orders`}>
-                    訂購紀錄
-                </Link>
+                    </li>
+                    {props.drinkData && props.drinkData.menu &&
+                        props.drinkData.menu.map((series, key) => {
+                            return (
+                                <li className="nav-item">
+                                    <div key={key} className={"nav-link-background" + (path[2] == key? " selected" : "")}>
+                                        <Link className={"nav-link"} to={`/drinks/${key}`}>
+                                            {series.series}
+                                        </Link>
+                                    </div>
+                                </li>
+                            )
+                        })
+                    }
+                    <li className="nav-item">
+                        <div className={"nav-link-background " + (path[1] == "orders"? " selected" : "")}>
+                            <Link className="nav-link" to={`/orders`}>
+                                訂購紀錄
+                            </Link>
+                        </div>
+                    </li>
+                </ul>
             </div>
-        </div>
+        </nav>
     )
 }
