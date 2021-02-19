6db2478f4c1bde8b9526277712ec8039a7bd3c1d
Luke <apple894894@gmail.com>
2021-02-19 20:19:42 +0800

    update order layout

--- src/components/OrderSummary.js
+++ src/components/OrderSummary.js
@@ -12,8 +12,15 @@ export default function OrderSummary(props) {
         <div className="row mt-4">
             <h1>統計</h1>
             {props.data.map((item, key) => {
-                return (<div key={key} className="col-lg-3 col-sm-6">
-                    {`${item.item}(${item.size === "large"? "大": "中"}) /${item.sugar_tag}/${item.ice_tag}`} x {item.number}
+                return (<div key={key} className="col-lg-4 col-md-6 p-2">
+                    <div className="d-flex rounded border border-primary m-1 w-100 h-100">
+                        <div className="flex-grow-1 p-2">
+                            <span>{`${item.item}(${item.size === "large"? "大": "中"})/${item.sugar_tag}/${item.ice_tag}`}</span>
+                        </div>
+                        <div className="h-100 border-start border-primary p-2">
+                            <span>{item.number}</span>
+                        </div>
+                    </div>
                 </div>)
             })}
         </div>
