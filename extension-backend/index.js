b2e929fe22dc43c655347f400c143df050342a4f
jackraken <apple894894@gmail.com>
2021-09-30 04:05:34 +0800

    Add express server to host react app

---
+++ extension-backend/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Put all API endpoints under '/api'
+app.get('/api/hello', (req, res) => {
+
+  // Return them as json
+  res.json({
+    hello: "world"
+  });
+
+  console.log(`Sent ${count} passwords`);
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`listening on ${port}`);
