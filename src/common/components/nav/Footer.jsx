2c0743750bc4119c78f58eb08a6f63d4e78cfed1
jackraken <apple894894@gmail.com>
2022-09-03 04:07:00 +0800

    update footer

--- src/common/components/nav/Footer.jsx
+++ src/common/components/nav/Footer.jsx
@@ -1,17 +1,17 @@
+import GitHubButton from 'react-github-btn'
 
 import styles from "./Footer.module.css";
 
-export default function Footer (){
-    return <footer className={"bd-footer p-3 mt-5 text-center text-sm-start " + styles["sticky-footer"]} style={{
-      // position: "relative",
-      backgroundColor: "rgba(255,255,255,0.4)"
-      }}>
+export default function Footer() {
+  return <footer className={"bd-footer p-3 mt-5 text-center text-sm-start " + styles["sticky-footer"]} style={{
+    // position: "relative",
+    backgroundColor: "rgba(255,255,255,0.4)"
+  }}>
     <div className="container">
-      <ul className="bd-footer-links ps-0 mb-3">
-        <li className="d-inline-block"><a href="https://github.com/jackraken/datalab-drinks-frontend">GitHub</a></li>
-      </ul>
-      <div className="d-flex flex-row">
-        <p className="mb-0">Designed and built with all the love in the world by the <a href="http://www.cs.nthu.edu.tw/~shwu/">Datalab</a> with the help of <a href="https://www.camacafe.com/">cama coffee</a>(03)571-1500.</p>
+      <div className="d-flex flex-column gap-2 justify-content-center align-items-center">
+        <p className="mb-0">Designed and built with ❤️ by people from <a href="http://www.cs.nthu.edu.tw/~shwu/">NTHU Datalab</a></p>
+        <p>Menu from <a href="https://www.camacafe.com/Menu/1">cama coffee</a> (03)571-1500</p>
+        <GitHubButton href="https://github.com/Luke516/datalab-drinks-frontend" data-size="large" data-show-count="true" aria-label="Star Luke516/datalab-drinks-frontend on GitHub">Star</GitHubButton>
       </div>
     </div>
   </footer>
